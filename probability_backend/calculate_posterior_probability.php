67cd601f7627836ec087cc508ead40ba216609b8
nootanghimire <nootan.ghimire@gmail.com>
2015-08-09 17:03:14 +0545

    Improvised (just one function)

--- probability_backend/calculate_posterior_probability.php
+++ probability_backend/calculate_posterior_probability.php
@@ -54,10 +54,12 @@ while($row = mysqli_fetch_assoc($result_get_factors)){
 $input = $_POST['json_input'];
 $factors = json_decode($input, true); //true lets us use assoc. arrays
 
-foreach($factors as $factor){
+$list;
+/*foreach($factors as $factor){
 	//print_r($factor['name']);
 	$list[] = $factor['name'];
-}
+	break;
+}*/
 
 //get all individual probabilities
 $query ="SELECT * FROM probabilities_individual;";
@@ -72,25 +74,40 @@ foreach($factors as $factor){
 		//update the individual probability
 		foreach($factor['probabilities'] as $values => $probabilities)
 			$probability[$factor['name']][$values] = $probabilities;
-		calculate_probabilities($factor);
 		//continue; // :D
+}
+
+foreach($factors as $factor){
+	calculate_probabilities($factor);
 }
 
 function calculate_probabilities($single_factor_array){
 	global $list;
 	//valar morghulis - All functions must die
-	if(in_array($single_factor_array['name'], $list)) return;
+	if (isset($list)){
+		//echo "Yes! Found it! :D \n";
+		if(in_array($single_factor_array['name'], $list)) {
+			//echo "\n\nFound in list\n\n";
+			return;
+		}
+	}
 
+	//add  the current  factor in list
+	$list[] = $single_factor_array['name'];
+	//echo "We went there!";
 	//valar daheris - All functions must serve
 	if(is_cause($single_factor_array['name'])){
 		$f_s = $single_factor_array['name'];
+		echo "'".$f_s . "' is a cause. Operating on it's immediate Effects! \n ";
 		$f_v = $single_factor_array['probabilities'];
 		operate_on_effects($f_s, $f_v);
 	}
 
-	//valar daheris - All functions must serve
+	//valar daheris
+	//- All functions must serve
 	if(is_effect($single_factor_array['name'])){
 		$f_s = $single_factor_array['name'];
+		echo "'".$f_s . "' is a effect. Operating on it's immediate Causes! \n";
 		$f_v = $single_factor_array['probabilities'];
 		operate_on_causes($f_s, $f_v);
 	}
@@ -98,79 +115,140 @@ function calculate_probabilities($single_factor_array){
 }
 
 function operate_on_effects($factor_str, $factor_val){
+	global $con, $list, $probability;
 	$query = "SELECT effect from model WHERE cause='$factor_str';";
 	$result = mysqli_query($con, $query) or die("Error Selecting Effects: ". mysqli_error($con));
 	while($row = mysqli_fetch_assoc($result)){
+		//if(in_array($row['effect'], $list)) continue;
+		//needs amemndment here
+		//
+		//
+		//
+		//
+		//
+		//
+		echo "  Operating on ".$row['effect']." \n";
 		$effect['name'] = $row['effect'];
 		$query2 = "SELECT value from probabilities_individual WHERE factor='".$row['effect']."';";
 		$result2 = mysqli_query($con, $query2) or die("Error selecting vallues from a specific factor: ". mysqli_error($con));
 		while($row2 = mysqli_fetch_assoc($result2)){
 			$val = $row2['value'];
-			$p_e = $row2['probability'];
+			echo "Operating on subvalue: ". $val . "\n\n";
+			$p_e = $probability[$effect['name']][$val];
 			//get model id
-			$query3 = "SELECT id from model WHERE cause='$factor_str' AND effect=".$row['effect']."';";
-			$result3 = mysqli_query($con, $query3);
+			$query3 = "SELECT id from model WHERE cause='$factor_str' AND effect='".$row['effect']."';";
+			//echo $query3;
+			//echo "\n";
+			$result3 = mysqli_query($con, $query3) or die("Could not get model! ". mysqli_error($con));
 			$model_id = mysqli_fetch_assoc($result3)['id'];
+			$prob = 0;
 			$count = 0;
 			foreach($factor_val as $cause_value => $p_c){
+				//echo "P_C should be: ". $factor_val[$cause_value];
+				//echo "\n\n";
 				$query4 = "SELECT probability_cause_effect as pce FROM probabilities WHERE model_id=$model_id AND value_cause='$cause_value' AND value_effect='$val';";
 				$result4 = mysqli_query($con, $query4);
 				$p_c_e = mysqli_fetch_assoc($result4)['pce'];
-				$prob = $p_c_e * $p_e / $p_c ;
+
+				$query5 = "SELECT probability_cause_effect as pce_2, value_effect as v_e FROM probabilities WHERE model_id=$model_id AND value_cause='$cause_value';";
+				echo  $query5 . "\n";
+				$result5 =mysqli_query($con, $query5) or die("Error in line 153 : ". mysqli_error($con));
+				$sum = 0;
+				while($row5 = mysqli_fetch_assoc($result5)){
+					echo "--- pce_2=".$row5['pce_2']." | prob_independent: ".$probability[$effect['name']][$row5['v_e']]. "\n";
+					$sum += ($row5['pce_2'] * $probability[$effect['name']][$row5['v_e']]);
+				}
+				//check for sanity if sum is still zero, don't f*** up!
+				//
+				if($sum == 0) {$prob += 0;}
+				else{$prob += ($p_c_e * $p_e/$sum);}
+				echo "probability: " . $prob . "\n\n";
+				//$prob += $p_c_e * $p_e / $p_c ;
 				$count++;
 			}
+			echo "-----------------". $count ."\n\n";
 
 			//$query4  = "SELECT * FROM probabilities WHERE model_id=$model_id AND value_cau"
-			$probability[$effect['name']][$val] = $prob/$count;
-			$effect['probabilities'][$val] = $prob/$count;
+			if($prob == 0){
+				$val_p = 0;
+			} else {
+				$val_p = $prob/$count;
+			}
+			echo "[][]FINALPROB:: ". $val_p;
+			$probability_local[$effect['name']][$val] = $val_p; // $prob/$count;
+			$effect['probabilities'][$val] = $val_p; //$prob/$count;
 		}
+		$probability[$effect['name']] = $probability_local[$effect['name']];
+		calculate_probabilities($effect);
 	}
-	calculate_probabilities($effect);
+
 }
 
 function operate_on_causes($factor_str, $factor_val){
+	global $con, $list, $probability;
 	$query = "SELECT cause from model WHERE effect='$factor_str';";
 	$result = mysqli_query($con, $query) or die("Error Selecting Causes: ". mysqli_error($con));
 	while($row = mysqli_fetch_assoc($result)){
+		//if(in_array($row['cause'], $list)) continue;
+		echo "  Operating on ".$row['cause']." \n";
 		$cause['name'] = $row['cause'];
-		$query2 = "SELECT value from probabilities_individual WHERE factor='".$row['effect']."';";
+		$query2 = "SELECT value from probabilities_individual WHERE factor='".$row['cause']."';";
 		$result2 = mysqli_query($con, $query2) or die("Error selecting vallues from a specific factor: ". mysqli_error($con));
 		while($row2 = mysqli_fetch_assoc($result2)){
 			$val = $row2['value'];
-			$p_c = $row2['probability'];
+			$p_c = $probability[$cause['name']][$row2['value']];
 			//get model id
-			$query3 = "SELECT id from model WHERE effect='$factor_str' AND cause=".$row['effect']."';";
+			$query3 = "SELECT id from model WHERE effect='$factor_str' AND cause='".$row['cause']."';";
 			$result3 = mysqli_query($con, $query3);
 			$model_id = mysqli_fetch_assoc($result3)['id'];
 			$count = 0;
+			$prob_e_c = 0;
+			$prob_e = 0;
 			foreach($factor_val as $effect_value => $p_e){
+
 				$query4 = "SELECT probability_effect_cause as pec FROM probabilities WHERE model_id=$model_id AND value_cause='$val' AND value_effect='$effect_value';";
+				//echo $query4 . "\n";
+				//echo "P_E should be: ". $factor_val[$effect_value];
+				//echo "\n\n";
 				$result4 = mysqli_query($con, $query4);
 				$p_e_c = mysqli_fetch_assoc($result4)['pec'];
-				$prob = $p_e_c * $p_c / $p_e ;
+				//if($p_e == 0) $p_e = 0.00001; //normally when p_e is 0 p_e_c is 0
+				//$prob += $p_e_c * $p_c / $p_e ;
+				$prob_e_c += $p_e_c;
+				$prob_e += $p_e;
 				$count++;
 			}
-			$probability[$cause['name']][$val] = $prob/$count;
+			$prob_e_c = $prob_e_c;
+			$prob_e = $prob_e;
+			if($prob_e_c == 0){
+				$val_p = 0;
+			} else {
+				$val_p = $prob_e_c * $p_c / $prob_e;
+			}
+			$probability[$cause['name']][$val] = $val_p; //$prob/$count;
 			//$query4  = "SELECT * FROM probabilities WHERE model_id=$model_id AND value_cau"
-			$cause['probabilities'][$val] = $prob/$count;
+			$cause['probabilities'][$val] = $val_p; //$prob/$count;
 		}
+		calculate_probabilities($cause);
 	}
-	calculate_probabilities($effect);
+
 }
 
 
 
 function is_cause($factor_str){
+	global $con;
 	$query = "SELECT * FROM model WHERE cause='$factor_str';";
 	$result = mysqli_query($con, $query) or die("Exception in Finding if the factor is a cause: ". mysqli_error($con));
-	if(mysqli_affected_rows($con) < 1) return false;
+	if(mysqli_num_rows($result) < 1) return false;
 	return true;
 }
 
 function is_effect($factor_str){
+	global $con;
 	$query = "SELECT * FROM model WHERE effect='$factor_str';";
 	$result = mysqli_query($con, $query) or die("Exception in Finding if the factor is a cause: ". mysqli_error($con));
-	if(mysqli_affected_rows($con) < 1) return false;
+	if(mysqli_num_rows($result) < 1) return false;
 	return true;
 }
 
